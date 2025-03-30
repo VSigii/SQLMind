1332191fc567e7b38ddeffb19858a298efdf2823
Vishal Sigdel <itsvishalsigdel@gmail.com>
2025-03-30 22:24:56 +0545

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def get_response(user_query:str, db:SQLDatabase, chat_history:list):
         You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's database.
         Based on the table schema below, question, sql query and sql response, write a natural language response. You do not let user know
         that you are accessing the database. You reply saying "As far as i know". Only allow users to view the contents using SELECT.
-        Any query related to DDL and DML that modifies the existing database MUST NOT RUN.
+        Any query related to DDL and DML that modifies the existing database MUST NOT RUN, like CREATE,ALTER,DELETE,DROP,MODIFY and so on. 
 
         <SCHEMA>{schema}</SCHEMA>
         Conversation History: {chat_history}
